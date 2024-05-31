c1ef8abcb5ef0a0b8066f06ced712bba5385d912
rafaelfranca1 <rafaeldefranca750@gmail.com>
2024-05-31 15:51:18 -0300

    add: geração dinamica de cards

--- Pokedex Project/script.js
+++ Pokedex Project/script.js
@@ -1,25 +1,39 @@
-let mainTag = document.querySelector('.pokemon-list');
-let mainConteudo = "";
+var requestURL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=151";
+var request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+
+var nomePokemon = [];
+
+request.onload = function() {
+    var pokemons = request.response;
+    listaNomes(pokemons);
+    
+    let mainTag = document.querySelector('.pokemon-list');
+    let mainConteudo = "";
+    
+    for (let i = 1; i <= 151; i++) {
+        mainConteudo += `
+        <div class="pokemon-card">
+            <img class="pokemon-image" src="https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/${i}.png?raw=true" alt="Pokemon ${nomePokemon[i-1]}">
+            <p class="pokemon-number">#${i.toString().padStart(3, '0')}</p>
+            <h3 class="pokemon-name">${nomePokemon[i-1]}</h3>
+        </div>
+        `;
+    }
+    
+    mainTag.innerHTML = mainConteudo;
+}
+
+function listaNomes(jsonObj) {
+    var pokemons = jsonObj["results"];
+
+    for (var i = 0; i < pokemons.length; i++) {
+        nomePokemon.push(capitalizeFirstLetter(pokemons[i].name)); 
+    }
+}
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.substring(1);
 }
-
-fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=151')
-    .then(response => response.json())
-    .then(data => {
-        let pokemonNames = data.results.map(pokemon => capitalizeFirstLetter(pokemon.name));
-        
-        for (let i = 1; i <= 151; i++) {
-            mainConteudo += `
-            <div class="pokemon-card">
-                <img class="pokemon-image" src="https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/${i}.png?raw=true" alt="Pokemon ${pokemonNames[i-1]}">
-                <p class="pokemon-number">#${i}</p>
-                <h3 class="pokemon-name">${pokemonNames[i-1]}</h3>
-            </div>
-            `;
-        }
-    
-        mainTag.innerHTML = mainConteudo;
-    })
-    .catch(error => console.error('Erro:', error));
